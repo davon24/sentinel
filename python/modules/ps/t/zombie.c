87d84e40e6daf90167c2f4b2855478d0a0912c18
Karl Rink <karl@rink.us>
2020-10-01 21:10:37 -0700

    hmmm, ps, mac, linux

--- python/modules/ps/t/zombie.c
+++ python/modules/ps/t/zombie.c
@@ -1,4 +1,6 @@
 
+/* cc zombie.c -o zombie */
+
 #include <stdlib.h>
 #include <sys/types.h>
 #include <unistd.h>
@@ -7,7 +9,7 @@ int main ()
 {
   pid_t child_pid;
 
-  child_pid = fork ();
+  child_pid = fork();
   if (child_pid > 0) {
     sleep (60);
   }
