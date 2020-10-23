3cb73108bf68087bc009345d0a2660668e9df07b
Karl Rink <karl@rink.us>
2020-10-22 19:43:32 -0700

    .gitlab-ci.yml.19

--- pkg/pkg_rpm.sh
+++ pkg/pkg_rpm.sh
@@ -2,22 +2,22 @@
 
 basedir=`dirname $0`
 
-ver=`awk '/^Version: / {print $2}' $basedir/sentinel.spec`
 
 mkdir -p ~/rpmbuild/{BUILD,RPMS,SOURCES,SPECS,SRPMS} >/dev/null 2>&1
 
 if [ ! -f ~/rpmbuild/SOURCES/sentinel-$ver.tar.gz ]; then
-
   curl -k https://gitlab.com/krink/sentinel/-/archive/master/sentinel-master.tar.gz >~/rpmbuild/SOURCES/sentinel-master.tar.gz
-  tar xvf ~/rpmbuild/SOURCES/sentinel-master.tar.gz -C ~/rpmbuild/SOURCES/
-  mv ~/rpmbuild/SOURCES/sentinel-master ~/rpmbuild/SOURCES/sentinel-$ver
-  cd ~/rpmbuild/SOURCES/
-  tar cvfz sentinel-$ver.tar.gz sentinel-$ver
-  cd -
-
 fi
 
-cp $basedir/sentinel.spec ~/rpmbuild/SPECS/sentinel.spec
+tar xvf ~/rpmbuild/SOURCES/sentinel-master.tar.gz -C ~/rpmbuild/SOURCES/
+
+ver=`awk '/^Version: / {print $2}' ~/rpmbuild/SOURCES/sentinel-master/pkg/sentinel.spec`
+cp ~/rpmbuild/SOURCES/sentinel-master/pkg/sentinel.spec ~/rpmbuild/SPECS/sentinel.spec
+
+mv ~/rpmbuild/SOURCES/sentinel-master ~/rpmbuild/SOURCES/sentinel-$ver
+cd ~/rpmbuild/SOURCES/
+tar cvfz sentinel-$ver.tar.gz sentinel-$ver
+cd -
 
 rpmbuild -tb ~/rpmbuild/SOURCES/sentinel-$ver.tar.gz
 #rpmbuild -ba ~/rpmbuild/SPECS/sentinel.spec
