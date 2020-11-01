2447ce10cf2ee21c107b6f24cfe2b3efd33d3842
Karl Rink <karl@rink.us>
2020-11-01 08:51:32 -0800

    nov 1 🍗

--- python/modules/gitegridy/gitegridy.py
+++ python/modules/gitegridy/gitegridy.py
@@ -6,10 +6,10 @@ from subprocess import Popen, PIPE
 
 import sys
 
-def gitStoreLink(git_store, List):
+def gitStoreLink(git_store, List, verbose=False):
 
     if not os.path.isdir(git_store):
-        print('mkdir ' + str(git_store))
+        if verbose: print('mkdir ' + str(git_store))
         os.mkdir(git_store, 0o755)
 
     for f in List:
@@ -17,56 +17,55 @@ def gitStoreLink(git_store, List):
         gfile = git_store + f
 
         if not os.path.isdir(os.path.dirname(gfile)):
-            print('mkdir ' + str(os.path.dirname(gfile)))
+            if verbose: print('mkdir ' + str(os.path.dirname(gfile)))
             os.mkdir(os.path.dirname(gfile), 0o755)
 
         if not os.path.isfile(gfile):
-            print('link ' + str(gfile))
+            if verbose: print('link ' + str(gfile))
             os.link(f, gfile)
 
     return True
 
-def gitStoreInit(git_store):
-    #os.chdir(git_store)
+def gitStoreInit(git_store, verbose=False):
+    os.chdir(git_store)
     if not os.path.isdir(git_store + '/.git'):
-        print('git init ' + str(git_store))
+        if verbose: print('git init ' + str(git_store))
         cmd = 'git init ' + str(git_store)
         proc = Popen(cmd.split(), stdout=PIPE, stderr=PIPE)
-        #out = proc.stdout.readlines()
-
-        for line in proc.stdout.readlines():
-            #print(line)
-            line = line.decode('utf-8').strip('\n')
-            print(line)
-
+        if verbose:
+            for line in proc.stdout.readlines():
+                print(line.decode('utf-8').strip('\n'))
     return True
 
-def gitStoreAdd(git_store, f):
+def gitStoreAdd(git_store, f, verbose=False):
     os.chdir(git_store)
-    print('git add ' + git_store + f)
+    if verbose: print('git add ' + git_store + f)
     cmd = 'git add ' + git_store + f
     proc = Popen(cmd.split(), stdout=PIPE, stderr=PIPE)
-    for line in proc.stdout.readlines():
-        print(line.decode('utf-8').strip('\n'))
-    return True
+    if verbose:
+        for line in proc.stdout.readlines():
+            print(line.decode('utf-8').strip('\n'))
+    return proc.stdout.readlines()
 
-def gitStoreCommit(git_store, f):
+def gitStoreCommit(git_store, f, verbose=False):
     os.chdir(git_store)
-    print('git commit -m "commit" ' + git_store + f)
+    if verbose: print('git commit -m "commit" ' + git_store + f)
     cmd = 'git commit -m "commit" ' + git_store + f
     proc = Popen(cmd.split(), stdout=PIPE, stderr=PIPE)
-    for line in proc.stdout.readlines():
-        print(line.decode('utf-8').strip('\n'))
-    return True
+    if verbose:
+        for line in proc.stdout.readlines():
+            print(line.decode('utf-8').strip('\n'))
+    return proc.stdout.readlines()
 
 
-def gitStoreStatus(git_store):
+def gitStoreStatus(git_store, verbose=False):
     os.chdir(git_store)
     cmd = 'git status'
     proc = Popen(cmd.split(), stdout=PIPE, stderr=PIPE)
-    for line in proc.stdout.readlines():
-        print(line.decode('utf-8').strip('\n'))
-    return True
+    if verbose:
+        for line in proc.stdout.readlines():
+            print(line.decode('utf-8').strip('\n'))
+    return proc.stdout.readlines()
 
 
 if __name__ == '__main__':
@@ -83,8 +82,16 @@ if __name__ == '__main__':
 
 
     if sys.argv[1:]:
+
         if sys.argv[1] == 'status':
-            git_status = gitStoreStatus(git_store)
+            git_status = gitStoreStatus(git_store, verbose=True)
+
+        if sys.argv[1] == 'add':
+            _file = sys.argv[2]
+            git_link = gitStoreLink(git_store, [_file], verbose=True)
+            git_add  = gitStoreAdd(git_store, _file, verbose=True)
+            git_commit = gitStoreCommit(git_store, _file, verbose=True)
+
 
 
 # git + tegridy
