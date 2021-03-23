a5b2ed975f30fc940095d4912c5452dad50eb7ba
Karl <karl.rink@nationsinfocorp.com>
2021-03-23 12:43:57 -0700

    1.6.20-1.dev-20210323-2 i think i'm done w/ this part

--- docs/dev/t.pdata.1.py
+++ docs/dev/t.pdata.1.py
@@ -1,39 +1,53 @@
 #!/usr/bin/env python3
 
-match_this = 'expire'
+#promKey = 'expire'
 
 s = 'sentinel_watch_syslog_rule_engine{config="watch-syslog",rule="rule-X",b2sum="2ed2fc412bfa54650d00fc5092b06ea320abf563",seen="True",data="LQM-WiFi: (5G) txRTSFrm=24 {txUcast=16} { } rxACKUcast=7",expire="3600",date="2021-03-23 10:28:07"} 2'
+#s = 'sentinel_watch_syslog_rule_engine{config="watch-syslog",rule="rule-X",b2sum="2ed2fc412bfa54650d00fc5092b06ea320abf563",seen="True",data="LQM-WiFi: (5G) txRTSFrm=24 {txUcast=16} { } rxACKUcast=7",date="2021-03-23 10:28:07"} 2'
 
-#data = s.lstrip("{")
-#import re
-#data = re.sub(r'^.*?{', '', s)
 
-#slice it like a samari.
+def promDataParser(promKey, promData):
+    rtn = None
 
-data = s
-data = data[data.find('{'):]
-data = data.lstrip('{')
-data = data[:data.rfind('}')] #right find!
-data = data.split(',')
+    if isinstance(promData, list):
+        promData = promData[0]
 
-#data is a list now...
-#for item in data:
-#    #item.split('=')
-#    key = item.split('=')[0]
-#    val = item.split('=')[1]
-#    #print(key, val)
-#    if key == match_this:
-#        print(key, val)
+    data = promData
 
-for item in data:
-    #print(item)
-    #key = item.split('=')[0]
-    #val = item.split('=')[1] #equal in the data!
-    i = item.split('=', 1) #max split 1
-    key = i[0]
-    val = i[1]
-    print(key, val)
+    try:
 
-#print(data)
+        data = data[data.find('{'):]
+        data = data.lstrip('{')
+        data = data[:data.rfind('}')] #right find!
+        data = data.split(',')
+
+        #data is a list now...
+        for item in data:
+            #key = item.split('=')[0]
+            #val = item.split('=')[1] #if the val has '=' in it!
+            i = item.split('=', 1) #max split 1
+            key = i[0]
+            val = i[1]
+
+            val = val.lstrip('"')
+            val = val.rstrip('"')
+
+            #print(key, val)
+            if key == promKey:
+                #print(key, val)
+                rtn = val
+    except IndexError as e:
+        return None
+
+    return rtn
+
+
+_expire = promDataParser('data', s)
+
+if _expire:
+    print('_expire from gDict ' + str(_expire))
+
+
+
 
 
