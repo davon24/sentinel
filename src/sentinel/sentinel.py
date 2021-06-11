ae69fd944823576d83cda4299a238ee7d72eda6b
Karl Rink <karl@rink.us>
2021-06-10 22:13:52 -0700

    1.7.2

--- src/sentinel/sentinel.py
+++ src/sentinel/sentinel.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python3
 
-
 import sys
 import os
-import json
 
-from . import tools
-from . import store
+PACKAGE_PARENT = '.'
+SCRIPT_DIR = os.path.dirname(os.path.realpath(os.path.join(os.getcwd(), os.path.expanduser(__file__))))
+sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
+
+from tools import *
+from store import *
+
+import json
 
 __version__ = tools.__version__
 
